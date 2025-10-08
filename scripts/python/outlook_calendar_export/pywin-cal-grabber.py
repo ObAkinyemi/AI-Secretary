c69b8a73386ccab5a2e067d3df8c24fa9b53ac3f
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-10-08 06:18:24 -0600

    I have a working python script that actually grabs my ics data for the week. It prints it. Now I just need to try the version that turns the data into an ics.

--- scripts/python/outlook_calendar_export/pywin-cal-grabber.py
+++ scripts/python/outlook_calendar_export/pywin-cal-grabber.py
@@ -10,13 +10,13 @@ end = datetime.datetime(2025, 10, 10)
 # code for if I wanted to just do 7 days automatically
 # end_date = start_date + datetime.timedelta(days=7)
 
-# # Restrict items to date range
-# items = calendar.Items
-# items.Sort("[Start]")
-# items.IncludeRecurrences = True
-# restriction = f"[Start] >= '{start.strftime('%m/%d/%Y %H:%M %p')}' AND [End] <= '{end.strftime('%m/%d/%Y %H:%M %p')}'"
-# restricted_items = items.Restrict(restriction)
+# Restrict items to date range
+items = calendar.Items
+items.Sort("[Start]")
+items.IncludeRecurrences = True
+restriction = f"[Start] >= '{start.strftime('%m/%d/%Y %H:%M %p')}' AND [End] <= '{end.strftime('%m/%d/%Y %H:%M %p')}'"
+restricted_items = items.Restrict(restriction)
 
-# # Print subject and start time
-# for item in restricted_items:
-#     print(item.Subject, item.Start, item.end);
+# Print subject and start time
+for item in restricted_items:
+    print(item.Subject, item.Start, item.end);
